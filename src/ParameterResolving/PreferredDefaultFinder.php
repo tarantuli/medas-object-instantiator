696494d098e4939658fff382d6d245f452e3193c
Patrick Morpey <patrick@morphp.nl>
2023-07-13 09:34:58 +0200

    Moved PreferredDefault to core

--- src/ParameterResolving/PreferredDefaultFinder.php
+++ src/ParameterResolving/PreferredDefaultFinder.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace Medas\ObjectInstantiator\ParameterResolving;
 
-use Medas\Core\{Attributes\Service, Interfaces\ParameterResolver};
-use Medas\ObjectInstantiator\Attributes\PreferredDefault;
+use Medas\Core\{Attributes\Service,Interfaces\ParameterResolver};
+use Medas\Core\Attributes\PreferredDefault;
 
 #[Service]
 class PreferredDefaultFinder implements ParameterResolver
